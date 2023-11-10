9b9cea34516d02697eb1d03be5164445a18451e5
JW de Roos <janwillem@jwdr.nl>
2023-11-11 00:10:58 +0100

    Update __init__.py

--- components/christmaslights/__init__.py
+++ components/christmaslights/__init__.py
@@ -4,21 +4,25 @@ import esphome.config_validation as cv
 from esphome.components import output
 from esphome.const import CONF_OUTPUT_ID, CONF_OUTPUT
 
+CONF_STRAND_1 = "strand1"
+CONF_STRAND_2 = "strand2"
+
+
 christmas_lights_ns = cg.esphome_ns.namespace('christmas_lights')
 ChristmasLights = christmas_lights_ns.class_('ChristmasLights', cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ChristmasLights),
-    cv.Required("strand1"): output.FloatOutput,
-    cv.Required("strand2"): output.FloatOutput
+    cv.Required(CONF_STRAND_1): output.FloatOutput,
+    cv.Required(CONF_STRAND_2): output.FloatOutput
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
     yield cg.register_component(var, config)
 
-    strand1 = yield cg.get_variable(config[CONF_OUTPUT])
+    strand1 = yield cg.get_variable(config[CONF_STRAND_1])
     cg.add(var.set_strand1(strand1))
 
-    strand2 = yield cg.get_variable(config[CONF_OUTPUT])
+    strand2 = yield cg.get_variable(config[CONF_STRAND_2])
     cg.add(var.set_strand2(strand2))
