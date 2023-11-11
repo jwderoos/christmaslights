2369a6d1cd6cac0814de6b6f90f9cbac27a768ac
Jan Willem de Roos <janwillem@jwdr.nl>
2023-11-12 00:47:41 +0100

    adds a dump config of sorts...

--- components/christmaslights/christmaslights.cpp
+++ components/christmaslights/christmaslights.cpp
@@ -13,17 +13,19 @@ namespace esphome {
                   // module = new ModeController(strand1_, strand2_, 250);
                   // module = new Alternate(strand1_, strand2_, 5); //steady on
                   // module = new Alternate(strand1_, strand2_, 400);
-                  module_ = new AlternateTwinkle(strand1_, strand2_, 800);
-                  module_->init();
+                  // module_ = new AlternateTwinkle(strand1_, strand2_, 800);
+                  // module_->init();
+                  strand1_->turn_on();
             }
 
             void ChristmasLights::loop() {
-                  module_->step();
+                  // module_->step();
             }
 
 
             void ChristmasLights::dump_config(){
-            //     ESP_LOGCONFIG(TAG, "Empty custom light");
+                  ESP_LOGCONFIG("strand1", "FloatOutput");
+                  ESP_LOGCONFIG("strand2", "FloatOutput");
             }
 
       } 
